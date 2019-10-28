a286abcce809108f2dc44b9aa6bdc949b9394c1c
Epicpkmn11 <epicpkmn11@outlook.com>
2019-10-28 12:41:11 -0500

    Use bank box for top screen filtering

--- arm9/source/manager.cpp
+++ arm9/source/manager.cpp
@@ -319,7 +319,7 @@ void drawBox(bool top) {
 			loadPokemonSprite(getPokemonIndex(tempPkm), bmp);
 			fillSpriteImage(i, top, bmp);
 			setSpriteVisibility(i, top, true);
-			if(*save->pkm(currentSaveBox, i) == *filter) {
+			if(*save->pkm(top ? currentBankBox : currentSaveBox, i) == *filter) {
 				setSpriteAlpha(top, i, 15);
 			} else {
 				setSpriteAlpha(top, i, 8);
