ddeb891affc58e7095d9d00601e3012c73590fbd
Epicpkmn11 <epicpkmn11@outlook.com>
2020-01-18 04:03:39 -0600

    Add info prompt after dumping Pokémon

--- arm9/include/input.hpp
+++ arm9/include/input.hpp
@@ -5,10 +5,14 @@
 namespace Input {
 	std::string getLine();
 	std::string getLine(unsigned maxLength);
+
 	int getInt();
 	int getInt(unsigned max);
+
 	bool getBool();
 	bool getBool(std::string option1, std::string option2);
+
+	void prompt(std::string message, const std::string &confirm);
 }
 
 #endif
